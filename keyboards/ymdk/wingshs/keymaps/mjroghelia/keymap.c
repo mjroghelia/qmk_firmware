68270e5701ed72295efea31c3e5b0c99a3b90285
Mark Roghelia <mark@roboshark.com>
2021-10-17 19:41:03 -0400

    Keymap updates

--- keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
+++ keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
@@ -29,11 +29,11 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[WIN_BASE] = LAYOUT_all(
-		KC_GESC,     KC_1,    KC_2,    KC_3,         KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_DEL,
-		KC_TAB,      KC_Q,    KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_GRV,
+		KC_GRV,      KC_1,    KC_2,    KC_3,         KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_DEL,
+		KC_TAB,      KC_Q,    KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_NO,
 		MO(WIN_NAV), KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  TG(MEDIA_ETC),
 		KC_LSFT,     KC_Z,    KC_X,    KC_C,         KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL,     KC_LALT, KC_LCTL, MO(WIN_APPS), KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+		KC_LCTL,     KC_LALT, KC_LCTL, LT(WIN_APPS, KC_ESC), KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[WIN_NAV] = LAYOUT_all(
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[MAC_BASE] = LAYOUT_all(
-		KC_GESC,     KC_1,    KC_2,    KC_3,  KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
-		KC_TAB,      KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-		MO(MAC_NAV), KC_A,    KC_S,    KC_D,  KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  TG(MEDIA_ETC),
-		KC_LSFT,     KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL,     KC_LGUI, KC_LGUI, KC_NO, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+		KC_GRV,      KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+		KC_TAB,      KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
+		MO(MAC_NAV), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  TG(MEDIA_ETC),
+		KC_LSFT,     KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
+		KC_LCTL,     KC_LGUI, KC_LGUI, KC_ESC, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	
 	[MAC_NAV] = LAYOUT_all(
