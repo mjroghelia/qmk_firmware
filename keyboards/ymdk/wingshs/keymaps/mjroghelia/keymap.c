eb51c623596c8a2183ed07b1876d2f85c8a9c68d
Mark Roghelia <mark@roboshark.com>
2021-11-07 08:17:27 -0500

    Make WIN on by default

--- keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
+++ keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, TT(MEDIA_ETC),
 		KC_NO,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_GRV,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL, KC_LALT, KC_LCTL, KC_SPC, KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+		KC_LCTL, KC_LALT, KC_SPC,  KC_NO,  KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[WIN] = LAYOUT_all(
@@ -148,3 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO, KC_NO,    KC_NO,   KC_NO,       KC_NO,       KC_NO,    KC_NO, KC_NO, KC_NO
 	)
 };
+
+void keyboard_post_init_user(void) {
+	layer_move(WIN);
+}
