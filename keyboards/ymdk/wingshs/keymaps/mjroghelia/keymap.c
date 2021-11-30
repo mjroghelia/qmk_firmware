60d8b5c42937cb6f2680fdcf7343c1027bdfde19
Mark Roghelia <mark@roboshark.com>
2021-11-30 00:29:38 -0500

    Separate ADMIN, Grave on FN, mouse experiment

--- keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
+++ keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
@@ -57,6 +57,7 @@ enum layers {
 	MAC,
 	TEST,
 	FN,
+	ADMIN,
 	WIN_NAV,
 	MAC_NAV,
 	APPS,
@@ -69,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[BASE] = LAYOUT_all(
 		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, TG(FN),
-		KC_NO,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_GRV,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, TG(ADMIN),
+		KC_NO,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  TG(FN),
 		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
 		KC_LCTL, KC_LALT, KC_SPC,  MO(FN), KC_SPC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
 	),
@@ -100,11 +101,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[FN] = LAYOUT_all(
-		TG(FN), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
-		KC_NO,  KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_TRNS,
-		KC_NO,  KC_MPRV, KC_MRWD, KC_MFFD, KC_MNXT, KC_MPLY, KC_NO,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  RESET,
-		KC_NO,  KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   CK_WIN,  TG(TEST), CK_MAC
+		KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  KC_NO,   KC_NO,
+		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_NO,   KC_BTN2, KC_MPLY, KC_NO,  KC_NO,   KC_NO,   KC_NO,
+		KC_NO,  KC_MPRV, KC_MRWD, KC_MFFD, KC_MNXT, KC_MPLY, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   KC_NO,  KC_NO,   KC_TRNS,
+		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE, KC_WH_D, KC_WH_U, KC_NO,   KC_NO,  KC_VOLU,
+		KC_NO,  KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_MPRV, KC_VOLD, KC_MNXT
+	),
+
+	[ADMIN] = LAYOUT_all(
+		TG(ADMIN), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TG(ADMIN),
+		KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, RESET,
+		KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, CK_WIN, TG(TEST), CK_MAC
 	),
 
 	[WIN_NAV] = LAYOUT_all(
