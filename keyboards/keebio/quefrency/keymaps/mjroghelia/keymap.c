54fc75405c238668d50f0c876915bae426cad13c
Mark Roghelia <mjroghelia@github.com>
2022-03-07 13:51:33 -0500

    More FN changes

--- keyboards/keebio/quefrency/keymaps/mjroghelia/keymap.c
+++ keyboards/keebio/quefrency/keymaps/mjroghelia/keymap.c
@@ -59,7 +59,6 @@ enum layers {
 	MAC,
 	FN,
 	SYM,
-	ADMIN,
 	WIN_NAV,
 	MAC_NAV
 };
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,  KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,  KC_DEL,  KC_BSPC,   KC_DEL,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,  KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,          KC_RBRC, KC_BSLS, KC_PGUP,
 		KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,  KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, LT(SYM, KC_QUOT), KC_ENT,  KC_PGDN,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,  KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO(ADMIN),
+		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,  KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO(FN),
 		KC_LCTL, KC_LGUI, KC_LALT, MO(FN), KC_SPC, KC_NO, LT(FN, KC_SPC), KC_RGUI, KC_RCTL, KC_NO,   KC_LEFT, KC_DOWN,          KC_RGHT
 	),
 
@@ -92,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[FN] = LAYOUT_65(
 		KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, KC_NO, KC_NO, KC_NO,
-		KC_NO,  KC_NO,   CK_APP1, CK_APP2, CK_APP3, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPLY, KC_NO,  KC_NO,  KC_NO, KC_NO,
+		KC_NO,  KC_NO,   CK_APP1, CK_APP2, CK_APP3, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPLY, KC_NO,  KC_NO,  CK_WIN, KC_NO,
 		KC_NO,  CK_NOTN, CK_APP4, CK_APP5, CK_APP6, KC_NO, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPRV, KC_NO,   KC_NO,  KC_NO,  KC_NO,
-		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_MUTE, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
+		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_MUTE, KC_NO,   KC_NO,   CK_MAC,   KC_NO,  KC_NO,  KC_NO,
 		KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO
 	),
 
@@ -104,14 +103,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		S(KC_8),   S(KC_GRV),  S(KC_4), S(KC_9),   S(KC_0), KC_LBRC,    KC_RBRC,    KC_GRV,  S(KC_QUOT), KC_QUOT,   S(KC_SCLN), S(KC_QUOT), KC_NO,     KC_NO,
 		KC_NO,     KC_BSLS,    KC_NO,   KC_NO,     KC_NO,   KC_NO,      KC_NO,      KC_NO,   KC_NO,      KC_NO,     KC_NO,      KC_NO,      KC_NO,     KC_NO,
 		KC_NO,     KC_NO,      KC_NO,   KC_NO,     KC_SPC,  KC_NO,      KC_TRNS,    KC_NO,   KC_NO,      KC_NO,     KC_NO,      KC_NO,      KC_NO
-	),
-
-	[ADMIN] = LAYOUT_65(
-		TG(ADMIN), KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		KC_NO,     KC_NO, CK_WIN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		KC_NO,     KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		KC_NO,     KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, CK_MAC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-		KC_NO,     KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
 	),
 
 	[WIN_NAV] = LAYOUT_65(
