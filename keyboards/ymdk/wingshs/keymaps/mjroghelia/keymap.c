108cc2b2d1bf567b8682ed1b321e27da1696c93f
Mark Roghelia <mjroghelia@github.com>
2022-04-01 01:31:09 -0400

    more symbol layer work

--- keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
+++ keyboards/ymdk/wingshs/keymaps/mjroghelia/keymap.c
@@ -42,11 +42,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[BASE] = LAYOUT_all(
-		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,            KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_DEL,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,            KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-		KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,            KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,            KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL, KC_LALT, KC_SPC,  MO(FN), LT(SYM, KC_SPC), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+		KC_GESC, KC_1,    KC_2,          KC_3,   KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,            KC_0,    KC_MINS,          KC_EQL,  KC_BSPC,   KC_DEL,
+		KC_TAB,  KC_Q,    KC_W,          KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,            KC_P,    KC_LBRC,          KC_RBRC, KC_BSLS, KC_PGUP,
+		KC_ESC,  KC_A,    KC_S,          KC_D,   KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,            KC_SCLN, LT(SYM, KC_QUOT), KC_ENT,  KC_PGDN,
+		KC_LSFT, KC_Z,    LT(SYM, KC_X), KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, LT(SYM, KC_DOT), KC_SLSH, KC_RSFT,          KC_UP,
+		KC_LCTL, KC_LALT, KC_SPC,        MO(FN), KC_SPC, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[WIN] = LAYOUT_all(
@@ -75,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	/*
 	+ < = > - { } & | _
-    ~ ! * ( ) [ ] ` " ' :
-	  \ @ # $ % ^   < > ?
+    ~ @ ! ( ) [ ] ` ' :
+	      # $ % ^ * "   \
 	*/
 	[SYM] = LAYOUT_all(
-		KC_GRV,    S(KC_1),    S(KC_2), S(KC_3),   S(KC_4), S(KC_5),    S(KC_6),    S(KC_7), S(KC_8),    S(KC_9),    S(KC_0),    S(KC_MINS), S(KC_EQL), KC_TRNS, KC_TRNS,
-		S(KC_EQL), S(KC_COMM), KC_EQL,  S(KC_DOT), KC_MINS, S(KC_LBRC), S(KC_RBRC), S(KC_7), S(KC_BSLS), S(KC_DOT),  S(KC_MINS), KC_NO,      KC_NO,     KC_NO, KC_NO,
-		S(KC_GRV), S(KC_1),    S(KC_8), S(KC_9),   S(KC_0), KC_LBRC,    KC_RBRC,    KC_GRV,  KC_QUOT,    S(KC_QUOT), S(KC_SCLN), KC_TRNS,    KC_NO,     KC_NO,
-		KC_NO,     KC_BSLS,    S(KC_2), S(KC_3),   S(KC_4), S(KC_5),    S(KC_6),    KC_NO,   S(KC_COMM), S(KC_DOT),  S(KC_SLSH), KC_NO,      KC_NO,
+		KC_GRV,    S(KC_1),    S(KC_2), S(KC_3),   S(KC_4), S(KC_5),    S(KC_6),    S(KC_7), S(KC_8),    S(KC_9),   S(KC_0),    S(KC_MINS), S(KC_EQL), KC_TRNS, KC_TRNS,
+		S(KC_EQL), S(KC_COMM), KC_EQL,  S(KC_DOT), KC_MINS, S(KC_LBRC), S(KC_RBRC), S(KC_7), S(KC_BSLS), S(KC_DOT), S(KC_MINS), KC_NO,      KC_NO,     KC_NO, KC_NO,
+		S(KC_GRV), S(KC_2),    S(KC_1), S(KC_9),   S(KC_0), KC_LBRC,    KC_RBRC,    KC_GRV,  KC_QUOT,    KC_NO,     S(KC_SCLN), KC_TRNS,    KC_NO,     KC_NO,
+		KC_NO,     KC_TRNS,    KC_TRNS, S(KC_3),   S(KC_4), S(KC_5),    S(KC_6),    S(KC_8), S(KC_QUOT), KC_TRNS,   KC_BSLS,    KC_NO,      KC_NO,
 		KC_NO,     KC_NO,      KC_SPC,  KC_NO,     KC_TRNS, KC_NO,      KC_TRNS,    KC_NO,   KC_NO
 	),
 
