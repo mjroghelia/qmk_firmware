aa69ec614cf976728205647a2650a8c2f8da3f7d
Mark Roghelia <mark@roboshark.com>
2024-02-18 00:00:28 -0500

    Fix backspace

--- keyboards/keebio/iris/keymaps/mjroghelia/keymap.c
+++ keyboards/keebio/iris/keymaps/mjroghelia/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CK_TAB,           KC_NO,            CK_APP1,          CK_APP2,          CK_APP3,          KC_NO,            KC_NO,            KC_NO,            KC_PGDN,          KC_PGUP,          KC_MPLY,          KC_NO,
         CK_CLR,           CK_NOTN,          CK_APP4,          CK_APP5,          CK_APP6,          CK_APP7,          KC_LEFT,          KC_DOWN,          KC_UP,            KC_RGHT,          KC_ENT,           KC_GRV,
         KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            MO(ADMIN),        KC_NO,            KC_NO,            KC_MUTE,          KC_NO,            KC_NO,            KC_NO,            KC_NO,
-        KC_NO,            KC_NO,            KC_TRNS,          KC_BSPC,          KC_NO,            KC_MPRV
+        KC_NO,            KC_NO,            KC_TRNS,          KC_BSPC,          KC_BSPC,          KC_MPRV
     ),
 
     [NAV] = LAYOUT(
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,            KC_TILD,          KC_PLUS,          KC_MINS,          KC_UNDS,          KC_LCBR,          KC_CIRC,          KC_ASTR,          KC_LPRN,          KC_RPRN,          KC_DQUO,          KC_NO,
         CK_CLR,           KC_EXLM,          KC_LT,            KC_EQL,           KC_GT,            KC_RCBR,          KC_NO,            KC_PIPE,          KC_AMPR,          KC_DQUO,          KC_TRNS,          KC_DQUO,
         KC_LSFT,          KC_BSLS,          KC_PERC,          KC_ASTR,          KC_LBRC,          KC_RBRC,          KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_RSFT,
-        KC_NO,            KC_SPC,           KC_SPC,           KC_SPC,           KC_NO,            KC_NO
+        KC_NO,            KC_SPC,           KC_SPC,           KC_SPC,           KC_SPC,           KC_NO
     ),
 
     [NUM] = LAYOUT(
