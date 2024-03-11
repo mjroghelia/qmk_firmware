d7342118cadf86678d34aa0492d048ccc8c46c48
Mark Roghelia <mark@roboshark.com>
2024-03-11 00:37:22 -0400

    Remove an extra enter

--- keyboards/keebio/iris/keymaps/mjroghelia/keymap.c
+++ keyboards/keebio/iris/keymaps/mjroghelia/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_BSLS,
         LT(NAV, KC_ESC),  KC_A,             KC_S,             KC_D,             KC_F,             KC_G,             KC_H,             KC_J,             KC_K,             KC_L,             KC_SCLN,          KC_QUOT,
         OSM(MOD_LSFT),    KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_LALT,          KC_RALT,          KC_N,             KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          OSM(MOD_RSFT),
-        KC_LGUI,          KC_LCTL,          LT(FN, KC_ENT),   LT(SYM, KC_SPC),  LT(NUM, KC_ENT),  KC_RGUI
+        KC_LGUI,          KC_LCTL,          MO(FN),           LT(SYM, KC_SPC),  LT(NUM, KC_ENT),  KC_RGUI
     ),
 
     [MAC] = LAYOUT(
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_BSLS,
         LT(NAV, KC_ESC),  KC_A,             KC_S,             KC_D,             KC_F,             KC_G,             KC_H,             KC_J,             KC_K,             KC_L,             KC_SCLN,          KC_QUOT,
         OSM(MOD_LSFT),    KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_LALT,          KC_RALT,          KC_N,             KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          OSM(MOD_RSFT),
-        KC_LCTL,          KC_LGUI,          LT(FN, KC_ENT),   LT(SYM, KC_SPC),  LT(NUM, KC_ENT),  KC_RGUI
+        KC_LCTL,          KC_LGUI,          MO(FN),           LT(SYM, KC_SPC),  LT(NUM, KC_ENT),  KC_RGUI
     ),
 
     [FN] = LAYOUT(
