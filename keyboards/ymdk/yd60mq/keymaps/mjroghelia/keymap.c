77518a4eb34b05b12ab379c2839e80d958101db9
Mark Roghelia <mark@roboshark.com>
2024-10-12 14:29:26 -0400

    sync up yd60mq

--- keyboards/ymdk/yd60mq/keymaps/mjroghelia/keymap.c
+++ keyboards/ymdk/yd60mq/keymaps/mjroghelia/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_LBRC,          KC_RBRC,          KC_BSLS,
         LT(NAV, KC_ESC),  KC_A,             KC_S,             KC_D,             KC_F,             KC_G,             KC_H,             KC_J,             KC_K,             KC_L,             KC_SCLN,          KC_QUOT,          KC_NO,            KC_ENT,
         OSM(MOD_LSFT),    KC_NO,            KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_N,             KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          KC_NO,            OSM(MOD_RSFT),    MO(ADMIN),
-        KC_LCTL,          KC_LGUI,          KC_LALT,          KC_LCTL,          MO(FN),           LT(SYM, KC_SPC),  MO(NUM),          KC_LEFT,          KC_DOWN,          KC_UP,            KC_RGHT
+        KC_LCTL,          KC_LGUI,          KC_LALT,          KC_LCTL,          MO(FN),           LT(SYM, KC_SPC),  KC_RGUI,          KC_LEFT,          KC_DOWN,          KC_UP,            KC_RGHT
     ),
 
     [MAC] = LAYOUT_all(
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,             KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_LBRC,          KC_RBRC,          KC_BSLS,
         LT(NAV, KC_ESC),  KC_A,             KC_S,             KC_D,             KC_F,             KC_G,             KC_H,             KC_J,             KC_K,             KC_L,             KC_SCLN,          KC_QUOT,          KC_NO,            KC_ENT,
         OSM(MOD_LSFT),    KC_NO,            KC_Z,             KC_X,             KC_C,             KC_V,             KC_B,             KC_N,             KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          KC_NO,            OSM(MOD_RSFT),    MO(ADMIN),
-        KC_LCTL,          KC_LGUI,          KC_LALT,          KC_LGUI,          MO(FN),           LT(SYM, KC_SPC),  MO(NUM),          KC_LEFT,          KC_DOWN,          KC_UP,            KC_RGHT
+        KC_LCTL,          KC_LGUI,          KC_LALT,          KC_LGUI,          MO(FN),           LT(SYM, KC_SPC),  KC_RGUI,          KC_LEFT,          KC_DOWN,          KC_UP,            KC_RGHT
     ),
 
     [FN] = LAYOUT_all(
